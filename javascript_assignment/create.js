7e72c08926ca1b8b83ee9e24c2a2997674bdad01
raipriyanka271 <priyanka@cronj.com>
2019-06-26 23:16:59 +0530

    session created

--- javascript_assignment/create.js
+++ javascript_assignment/create.js
@@ -4,13 +4,12 @@ function emp() {
     var emps = [];
     var y = window.localStorage.getItem("emps");
     if (y) {
-        emps = JSON.parse(y);
-    }
+        emps = JSON.parse(y);}
     console.log(emps);
     var empname = document.getElementById("name").value;
     var empemail = document.getElementById("email").value;
     var empage = document.getElementById("age").value;
-    var empsal = document.getElementById("salary").value; 3
+    var empsal = document.getElementById("salary").value; 
     var emprem = document.getElementById("remark").value;
     if (empname == "")
         document.getElementById("namecheck").innerHTML = "Field Can Not Be Empty";
@@ -34,19 +33,18 @@ function emp() {
 
     else {
         emps.push({ "addname": empname, "addemail": empemail, "addage": empage, "addsalary": empsal, "addremark": emprem });
-        document.getElementById("name").value = " ";
-        document.getElementById("email").value = " ";
-        document.getElementById("age").value = " ";
-        document.getElementById("salary").value = " ";
-        document.getElementById("remark").value = " ";
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("salary").value = "";
+        document.getElementById("remark").value = "";
         document.getElementById("namecheck").innerHTML = " ";
         document.getElementById("emailcheck").innerHTML = " ";
         document.getElementById("agecheck").innerHTML= " ";
         document.getElementById("salcheck").innerHTML = " ";
         document.getElementById("remcheck").innerHTML = " ";
         console.log(emps);
-
-    }
+}
     function validateemail() {
         var Email = document.myform.email.value;
         var posat = Email.indexOf("@");
@@ -68,13 +66,7 @@ function emp() {
     }
     window.localStorage.setItem("emps", JSON.stringify(emps));
     console.log(emps);
-    
-
-
-
 }
-
-
 function searchEmp() {
     var emps = [];
     var y = window.localStorage.getItem("emps");
@@ -86,6 +78,7 @@ function searchEmp() {
         return (emp.addname === empname);
     }
     );
+
     text = "<tr>" + "<td>" + result[0].addname + "</td>"
         + "<td>" + result[0].addemail + "</td>"
         + "<td>" + result[0].addage + "</td>"
@@ -94,9 +87,6 @@ function searchEmp() {
     "</tr>"
 
     document.getElementById("res2").innerHTML = text;
-
-
-
 }
 function searchE() {
     var emps = [];
@@ -110,10 +100,7 @@ function searchE() {
     }
     );
     window.location = "edit2.html";
-
-
 }
-
 function createlist() {
 
     var emps = [];
@@ -136,8 +123,6 @@ function createlist() {
             "</tr>"
 
         }
-
-
         document.getElementById("result").innerHTML = text;
     }
 
