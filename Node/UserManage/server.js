de0bf1a052acccced8cdbc1f1a51b413630a63e1
raipriyanka271 <priyanka@cronj.com>
2019-07-18 10:52:06 +0530

    changes made in server.js

--- Node/UserManage/server.js
+++ Node/UserManage/server.js
@@ -5,15 +5,12 @@ app.use(express.json());
 var DateDiff = require('date-diff');
 const url = require('url');
 const querystring = require('querystring');
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/userdb', { useNewUrlParser: true });
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  var datetime = new Date();
-  console.log((datetime.toISOString().slice(0,10)));
+  console.log("connected");
 });
 
 var Schema = new mongoose.Schema(
@@ -24,7 +21,7 @@ var Schema = new mongoose.Schema(
     gender: { type: String },
     address: { type: String },
     profession: { type: String },
-     age:{type:Number}
+    age: { type: Number }
   })
 
 
@@ -39,13 +36,13 @@ function findage(dob) {
   console.log(dob)
   let current
   let date1 = new Date();
-  var date2 = new Date(dob); 
+  var date2 = new Date(dob);
   let diff = new DateDiff(date1, date2);
   console.log(date1);
   console.log(date2);
-  let result=diff.years();
-  return  Math.floor(result);
-  
+  let result = diff.years();
+  return Math.floor(result);
+
 }
 app.post('/create', (req, res) => {
   user.create({
@@ -66,23 +63,11 @@ app.post('/create', (req, res) => {
   res.send('saved');
 
 });
-app.post('/user', (req, res) => {
-let param1
-  user.find({
-  
 
-  }, function (err, response) {
-    if (err) return handleError(err);
-    else {
-      console.log('USER FOUND');
-    }
-  });
-  res.send('USER FOUND');
-
-});
 app.get('/find/:id', (req, res) => {
   user.find({
-    userId: req.params.id}, function (err, response) {
+    userId: req.params.id
+  }, function (err, response) {
     if (err) return handleError(err);
     else {
       res.send(response);
@@ -92,13 +77,13 @@ app.get('/find/:id', (req, res) => {
 
 
 app.get('/filterage/:age', (req, res) => {
-  user.find({ age: { $gte: req.params.age} }, function (err, response) {
-      if (err) return handleError(err);
-      else {
-        res.send(response);
-      }
-    });
+  user.find({ age: { $gte: req.params.age } }, function (err, response) {
+    if (err) return handleError(err);
+    else {
+      res.send(response);
+    }
   });
+});
 
 
 
@@ -128,7 +113,7 @@ app.get('/filterprof/:profession', (req, res) => {
 
 app.get('/filtercity/:address', (req, res) => {
   user.find({
-    address : { $regex: req.params.address , $options: 'i' }
+    address: { $regex: req.params.address, $options: 'i' }
   }, function (err, response) {
     if (err) return handleError(err);
     else {
@@ -172,4 +157,20 @@ app.get('/delete/:id', (req, res) => {
     if (err) return handleError(err);
   });
 });
+//For compund query
+
+// app.post('/userquery', (req, res) => {
+//   let URL = 'http://localhost:5000/user/param1?=val1&param2=val2';
+//   let parsedUrl = url.parse(URL);
+//   let parsedQs = querystring.parse(parsedUrl.query);
+//   Console.log(parsedQs)
+//   user.find({ }, function (err, response) {
+//       if (err) return handleError(err);
+//       else {
+//         console.log('USER FOUND');
+//       }
+//     });
+//   res.send('USER FOUND');
+
+// });
 app.listen(port)
